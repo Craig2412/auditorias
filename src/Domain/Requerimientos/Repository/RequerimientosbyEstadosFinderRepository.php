cc0f822cbebcdda191b1148888e1c70f14865b02
Craig2412 <aularalexander55@gmil.com>
2024-01-16 17:28:19 -0400

    actualizacion de dashboard

--- src/Domain/Requerimientos/Repository/RequerimientosbyEstadosFinderRepository.php
+++ src/Domain/Requerimientos/Repository/RequerimientosbyEstadosFinderRepository.php
@@ -25,7 +25,39 @@ final class RequerimientosbyEstadosFinderRepository
         ->group('requerimientos.id_estado');
 
         $query->where(['requerimientos.id_condicion' => 1]);
+        
+        $array = [
+            [
+                'total' => 0,
+                'estado' => 'PROCESADO'
+            ],
+            [
+                'total' => 0,
+                'estado' => 'ASIGNADO'
+            ],
+            [
+                'total' => 0,
+                'estado' => 'NUEVO'
+            ]
+        ];
 
-        return $query->execute()->fetchAll('assoc') ?: [];
+        for ($i=0; $i < count($query->execute()->fetchAll('assoc')) ; $i++) { 
+            switch ($query->execute()->fetchAll('assoc')[$i]['estado']) {
+                case 'NUEVO':
+                    $array[2]['total'] = $query->execute()->fetchAll('assoc')[$i]['total'];
+                    break;
+                
+                case 'ASIGNADO':
+                    $array[1]['total'] = $query->execute()->fetchAll('assoc')[$i]['total'];
+                    break;
+                
+                case 'PROCESADO':
+                    $array[0]['total'] = $query->execute()->fetchAll('assoc')[$i]['total'];
+                    break;
+                
+            }
+        }
+
+        return $array ?: [];
     }
 }
